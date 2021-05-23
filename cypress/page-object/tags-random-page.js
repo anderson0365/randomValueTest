91aa6f28b01eb197a61c01b6ae95a2946a67336d
Anderson Luis Cáceres Padilla <al.caceres@uniandes.edu.co>
2021-05-22 22:08:13 -0500

    Refactorizacion de codigo

--- cypress/page-object/tags-random-page.js
+++ cypress/page-object/tags-random-page.js
@@ -23,55 +23,6 @@ export class GhostTesterRandom{
         cy.get('a[href="#/tags/"]').first().click({force: true});
     }
 
-    selectPostsfromManageMenu(){
-        cy.get('a[href="#/posts/"]').first().click({force: true});
-    }
-
-    selectPagesFromManageMenu(){
-        cy.get('a[href="#/pages/"]').first().click({force: true});
-    }
-
-    createAPost(postTitle){
-        cy.get('a[href="#/editor/post/"]').first().click({force: true});
-        cy.get('textarea[placeholder="Post Title"]').focus().type(postTitle);
-        cy.get('div[data-placeholder="Begin writing your post..."]').focus();
-    }
-
-    createAPage(pageTitle){
-        cy.get('a[href="#/editor/page/"]').first().click({force: true});
-        cy.get('textarea[placeholder="Page Title"]').focus().type(pageTitle);
-        cy.get('div[data-placeholder="Begin writing your page..."]').focus();
-    }
-
-    selectAPostWithItsTitle(postTitle){
-        cy.contains(postTitle).click({force: true});
-    }
-
-    selectAPageWithItsTitle(pageTitle){
-        cy.contains(pageTitle).click({force: true});
-    }
-
-    publishAPost(){
-        cy.get('div.gh-publishmenu-trigger').click({force: true});
-        cy.get('button.gh-publishmenu-button').click();
-    }
-
-    associatedTagToPost(tagName){
-        cy.get('button.post-settings').click();
-        cy.get('#tag-input').focus().type(tagName + '{enter}');
-        cy.get('button.close').click();
-    }
-
-    detachLastTagFromPost(){
-        cy.get('button.post-settings').click();
-        cy.get('#tag-input').focus().type('{backspace}');
-        cy.get('button.close').click();
-    }
-
-    checkTagDontHavePostsRelated(tagSlug){
-        cy.get(`a.gh-tag-list-posts-count[href="#/tags/${tagSlug}/"] > span`).should('have.text','0 posts');
-    }
-
     createOrModifyTag(newName, color, slug, description, metaTitle, metaDescription, oldName){
         if(oldName) this.selectATagByItsName(oldName);
         else cy.get('a[href="#/tags/new/"]').click({force: true});
@@ -115,24 +66,4 @@ export class GhostTesterRandom{
         cy.get('button[class="gh-btn gh-btn-red gh-btn-icon ember-view"]').first().click()
     }
 
-    removeATag(tagSlug){
-        cy.get(`a[href="#/tags/${tagSlug.toLowerCase()}/"]`).first().click({force: true});
-        cy.get('button[class="gh-btn gh-btn-red gh-btn-icon mb15"]').click()
-        cy.get('button[class="gh-btn gh-btn-red gh-btn-icon ember-view"]').first().click()
-    }
-
-    validateTagAssociation(tagName, postTitle){
-        cy.get(`a[title="List posts tagged with '${tagName}'"]`).click({force: true});
-        cy.contains(postTitle);
-    }
-
-    validateTagAssociationWithPage(tagName){
-        cy.get(`a[title="List posts tagged with '${tagName}'"] > span`).should('have.text','1 post');
-    }
-
-    removePost(){
-        cy.get('button.post-settings').click();
-        cy.get('button.settings-menu-delete-button').click();
-        cy.get('button[class="gh-btn gh-btn-red gh-btn-icon ember-view"]').first().click();
-    }
 }
